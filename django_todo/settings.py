aba600f0c172e6785d65261c2e9d36647519fb5d
Al3xandruF <>
2023-05-21 11:06:06 +0000

    Add heroku link to allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['8000-al3xandruf-hello-django--hyn8xbhm73.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['https://git.heroku.com/hello-django-fsf.git']
 
 
 # Application definition
